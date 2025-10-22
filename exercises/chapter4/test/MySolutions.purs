0bd2f2d4fb40a1fcec6e2d499174fa3e268cf865
James Koenig <Koenig.James@live.com>
2025-10-22 16:59:47 -0700

    Chapter 4, Section 1, Exercise 1 factorial sol'n

--- exercises/chapter4/test/MySolutions.purs
+++ exercises/chapter4/test/MySolutions.purs
@@ -1,1 +1,11 @@
 module Test.MySolutions where
+
+import Prelude
+
+--based upon statements in the assignment we're going to assume we're not facing
+-- negatives.  It'd be nice 
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+
