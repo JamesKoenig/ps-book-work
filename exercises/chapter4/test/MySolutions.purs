7005fad386260723c5a8111833c803d114788f36
James Koenig <Koenig.James@live.com>
2025-10-22 18:08:31 -0700

    add exercise description and comment

--- exercises/chapter4/test/MySolutions.purs
+++ exercises/chapter4/test/MySolutions.purs
@@ -2,8 +2,14 @@ module Test.MySolutions where
 
 import Prelude
 
+-- 1. (Easy) Write the `factorial` function using pattern matching.
+--   Hint: Consider the two corner cases of zero and non-zero inputs.
+--   Note: This is a repeat of an example from the previous chapter,
+--         but see if you can rewrite it here on your own.
+
 --based upon statements in the assignment we're going to assume we're not facing
--- negatives.  It'd be nice 
+-- negatives.  There's probably some world where you could type constraint it
+-- using some really cool System F type shit.
 factorial :: Int -> Int
 factorial 0 = 1
 factorial n = n * factorial (n-1)
