8c56ba3f5032774aa966d99defe0dbfd1f4f6192
James Koenig <Koenig.James@live.com>
2025-10-27 16:10:26 -0700

    add exercise descriptions

--- exercises/chapter5/src/Solutions/Group1.purs
+++ exercises/chapter5/src/Solutions/Group1.purs
@@ -2,6 +2,12 @@ module Solutions.Group1 where
 
 import Prelude
 
+-- 1. (Easy) Write a recursive function `isEven` that returns `true` if and only
+--           if its input is even
 isEven :: Int -> Boolean
 isEven n | n `mod` 2 == 0 = true
          | otherwise      = false
+
+-- 2. (Medium) Write a recursive function `countEven` that counts the number of
+--             even integers in an array.
+
