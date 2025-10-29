d1a2275a1ed615bb30ed1684c566938c5a0147b4
James Koenig <Koenig.James@live.com>
2025-10-28 19:32:47 -0700

    Ch5Grp2Ex2 `keepNonNegative` array filtering fn

--- exercises/chapter5/src/Solutions/Group2.purs
+++ exercises/chapter5/src/Solutions/Group2.purs
@@ -1,8 +1,15 @@
 module Solutions.Group2 where
 
+import Data.Array (filter)
+
 import Prelude
 
 -- 1. (Easy) Write a function `squared` which calculates the squares of an array
 --           of numbers.
 squared :: Array Number -> Array Number
 squared = map (\x -> x*x)
+
+-- 2. (Easy) Write a function `keepNonNegative` which removes the negative 
+--           numbers from an array of numbers.
+keepNonNegative :: Array Number -> Array Number
+keepNonNegative = filter (_>=0.0)
--- exercises/chapter5/test/Main.purs
+++ exercises/chapter5/test/Main.purs
@@ -17,6 +17,7 @@ import Solutions.Group1 (isEven
                         ,countEven
                         )
 import Solutions.Group2 (squared
+                        ,keepNonNegative
                         )
 
 main :: Effect Unit
@@ -67,7 +68,6 @@ main =
         test "Calculate squares" do
           Assert.equal [ 0.0, 1.0, 4.0, 9.0, 10000.0 ]
             $ squared [ 0.0, 1.0, 2.0, 3.0, 100.0 ]
-    {-  Move this block comment starting point to enable more tests
       suite "Exercise - keepNonNegative" do
         test "Do nothing with empty array" do
           Assert.equal []
@@ -75,6 +75,7 @@ main =
         test "Filter negative numbers" do
           Assert.equal [ 0.0, 2.0, 3.0 ]
             $ keepNonNegative [ -1.5, -1.0, 0.0, -0.1, 2.0, 3.0, -4.0 ]
+    {-  Move this block comment starting point to enable more tests
       suite "Exercise - <$?> infix operator for filter" do
         test "Define <$?> operator for filter" do
           Assert.equal [ 1, 1 ]
