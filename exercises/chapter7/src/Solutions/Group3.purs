d6fb61bb57d737d7ec061433610baac4dac06368
James Koenig <Koenig.James@live.com>
2025-11-23 17:50:14 -0800

    partial work for Ch7 Grp3 Ex2, taking a break

--- exercises/chapter7/src/Solutions/Group3.purs
+++ exercises/chapter7/src/Solutions/Group3.purs
@@ -3,7 +3,11 @@ module Solutions.Group3 where
 import Prelude
 import Data.Generic.Rep  (class Generic)
 import Data.Show.Generic (genericShow)
--- import Data.Eq.Generic   (genericEq)
+import Data.Foldable     (class Foldable
+                         ,foldr
+                         ,foldl
+                         ,foldMap
+                         )
 
 -- 1. (Easy) Write an `Eq` and `Show` instance for the following binary tree
 --           data structure
@@ -15,3 +19,25 @@ derive instance genericTree :: Generic (Tree a) _
 instance showTree :: Show a => Show (Tree a) where
   show tree = genericShow tree
 
+-- 2. (Medium) Write a `Traversable` instance for `Tree a`, which combines
+--             side-effects left-to-right
+
+-- Traverasable requires Functor and Foldable constraints
+instance Functor Tree where
+  map f Leaf = Leaf
+  map f (Branch left val right) = Branch (f <$> left) (f val) (f <$> right)
+
+-- we did this before in Ch6 Grp3 Ex5 & Ex6-- that was hard
+-- the instance fns for foldable are:
+--  foldr   :: forall a b. (a -> b -> b) -> b -> f a -> b
+--  foldl   :: forall a b. (b -> a -> b) -> b -> f a -> b
+--  foldMap :: forall a m. Monoid m => (a -> m) -> f a -> m
+instance Foldable Tree where
+-- get the easiest one oout of the way
+  foldMap fm Leaf = mempty
+  foldMap fm (Branch left x right) =
+    (foldMap fm left) <> (fm x) <> (foldMap fm right)
+  foldr f2 default _ = default
+  foldl f2 default _ = default
+  --foldr f2 default Leaf = default
+  --foldr f2 default (Branch left x right) =
