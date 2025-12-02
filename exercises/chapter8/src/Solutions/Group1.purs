106e0daf8a5a26ae1ffec0909653de46dc9ff5eb
James Koenig <Koenig.James@live.com>
2025-12-02 04:57:04 -0800

    third alternate using Data.Array.(!!)
    alternate solution for Ch8 Grp1 Ex1 `third`

--- exercises/chapter8/src/Solutions/Group1.purs
+++ exercises/chapter8/src/Solutions/Group1.purs
@@ -1,18 +1,27 @@
 module Solutions.Group1 where
 
-import Prelude
+import Prelude (bind
+               )
 
-import Data.Maybe
+import Data.Maybe (Maybe
+                  )
 import Data.Array (head
                   ,tail
+                  ,(!!)
                   )
 
 -- 1. (Easy) Write a function `third` that returns the third element of an array
 --           with three or more elements.  Your function should return an
 --           appropriate `Maybe` type.
 
-third :: forall (a :: Type). Array a -> Maybe a
-third as = do
+thirdM :: forall (a :: Type). Array a -> Maybe a
+thirdM as = do
   firstTail  <- tail as
   secondTail <- tail firstTail
   head secondTail 
+
+third' :: forall (a :: Type). Array a -> Maybe a
+third' = (_!!2)
+
+third :: forall ( a :: Type). Array a -> Maybe a
+third = thirdM --replace thirdM with third' to use (!!) ver
