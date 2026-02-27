a51fc9279bec24f97d55d5d060fad1151dbd9d20
James Koenig <Koenig.James@live.com>
2026-02-26 21:14:49 -0800

    Add problem text for Ch9 Grp3 Ex1-3

--- exercises/chapter9/src/Solutions/Group3.purs
+++ exercises/chapter9/src/Solutions/Group3.purs
@@ -16,6 +16,9 @@ import Affjax.Node as AN
 import Data.Time.Duration (Milliseconds(..))
 import Affjax.ResponseFormat (string)
 
+-- (Easy) Write a `concatenateManyParallel` function with the same signature
+--        as the earliere `concatenateMany` function but reads all input files
+--        in parallel
 concatenateManyParallel :: Array FilePath -> FilePath -> Aff Unit
 concatenateManyParallel inPaths outPath = do
   fileContents <- parTraverse (readTextFile UTF8) inPaths
@@ -24,7 +27,12 @@ concatenateManyParallel inPaths outPath = do
 
   writeTextFile UTF8 outPath toWrite
 
+-- (Medium) Write a:
 getWithTimeout :: Number -> String -> Aff (Maybe String)
+--          function which makes an HTTP `GET` request at the provided URL and
+--          returns either `Nothing` if the request takes longer than the
+--          provided timeout, or the string respnose if the request succeeds
+--          before the timeout elapses.
 getWithTimeout timeout url = do
   let ms = Milliseconds timeout
       req = AN.defaultRequest { url = url
@@ -38,3 +46,8 @@ getWithTimeout timeout url = do
   case result of
       Left _ -> pure Nothing
       Right response -> pure <<< Just $ response.body
+
+-- (Difficult) Write a `recurseFiles` function that takes a "root" file and
+--             returns an array of all paths listed in that file (and listed in
+--             the listed files too), Read the listed files in parallel. Paths
+--             are relative to the directory of the file they appear in.
