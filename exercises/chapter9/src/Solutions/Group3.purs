1d6d34955018976f3a158b5dae88783cac5abb25
James Koenig <Koenig.James@live.com>
2026-06-30 00:12:00 -0700

    Ch9 Grp3 Ex3 recurseFiles completed

--- exercises/chapter9/src/Solutions/Group3.purs
+++ exercises/chapter9/src/Solutions/Group3.purs
@@ -1,9 +1,11 @@
 module Solutions.Group3 ( concatenateManyParallel
                         , getWithTimeout
+                        , recurseFiles
                         ) where
 
 import Prelude
 import Node.Path (FilePath)
+import Node.Path as Path
 import Effect.Aff (Aff)
 import Control.Parallel (parTraverse)
 import Node.FS.Aff (readTextFile, writeTextFile)
@@ -14,7 +16,11 @@ import Data.HTTP.Method (Method(GET))
 import Affjax.Node as AN
 import Data.Time.Duration (Milliseconds(..))
 import Affjax.ResponseFormat (string)
+import Data.String.Regex (split)
+import Data.String.Regex.Unsafe (unsafeRegex)
+import Data.String.Regex.Flags (noFlags)
 import Data.Foldable (fold)
+import Data.Array (filter)
 
 -- (Easy) Write a `concatenateManyParallel` function with the same signature
 --        as the earliere `concatenateMany` function but reads all input files
@@ -51,3 +57,51 @@ getWithTimeout timeout url = do
 --             returns an array of all paths listed in that file (and listed in
 --             the listed files too), Read the listed files in parallel. Paths
 --             are relative to the directory of the file they appear in.
+
+fileLines :: String -> Array String
+fileLines = split $ unsafeRegex "\n" noFlags
+
+readAndPrepend :: FilePath -> Aff (Array FilePath)
+readAndPrepend file = do
+   content <- readTextFile UTF8 file
+   let lines = filter (_/="") $ fileLines content :: Array FilePath
+       here  = Path.dirname file :: FilePath
+       addContext :: FilePath -> FilePath
+       addContext x = Path.concat [here,x]
+       paths = addContext <$> lines
+
+   pure paths
+
+recurseFilesStep :: Array FilePath -> Aff (Array FilePath)
+recurseFilesStep files =
+    fold <$> parTraverse readAndPrepend files
+
+-- code is not working rn b/c recursefilesStep is prefixing the path
+-- onto empty files
+recurseFiles :: FilePath -> Aff (Array FilePath)
+recurseFiles root = go [root] [root]
+  where go :: Array FilePath -> Array FilePath -> Aff (Array FilePath)
+        go acc []   = pure acc
+        go acc todo = do
+          results <- recurseFilesStep todo
+          go (acc <> results) results
+
+-- Whiteboarding:
+-- e.g.
+--  file 1 is at location foo/bar/root.txt
+--  file 1 contains the line 'a.txt'
+--  file 2 is at location foo/bar/a.txt
+--  file 2 is empty.
+-- to start put foo/bar/root.txt at the top of a stack
+-- loop*:                (* Iterative, move to parallelization once understood)
+--    - remove the top element from the stack               (foo/bar/root.txt)
+--    - remember its dir path                               (foo/bar)
+--    - read its contents                                   ("a.txt")
+--    - split those contents by lines                       (["a.txt"])
+--    - for each of the lines:
+--      - if the line isn't empty:
+--        - concat the dir path to the line                 ("foo/bar/a.txt")
+--        - push the result onto a new stack (not necessary for map)
+--    - (dfs) push for-stack onto loop-stack
+--    OR
+--    - (bfs) append the for-stack after the loop-stack
